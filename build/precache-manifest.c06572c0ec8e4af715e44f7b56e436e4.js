f1f2d7d73c72d8684631187c0555670d008f0b1d
Vipin-0808 <vdass801@gmail.com>
2022-09-05 15:50:04 +0530

    fourth commit

---
+++ build/precache-manifest.c06572c0ec8e4af715e44f7b56e436e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22552bd18b3abca92963328e203eb81e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab52c4d9c0bc3afe0d30",
+    "url": "/static/css/main.2502858e.chunk.css"
+  },
+  {
+    "revision": "22daeca5b02b55968147",
+    "url": "/static/js/2.42d1fe8e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.42d1fe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab52c4d9c0bc3afe0d30",
+    "url": "/static/js/main.f971e86b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f971e86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4054bc057732f69f878d",
+    "url": "/static/js/runtime-main.13ba5424.js"
+  }
+]);
